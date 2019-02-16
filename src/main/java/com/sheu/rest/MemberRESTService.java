7ed017397924ecfd536a2476b4efda6ca1c23d43
Sheunesu Gumbie <sheunesu.gumbie@ebucks.com>
2019-02-16 22:14:48 +0200

    introduce rest services -1

--- src/main/java/com/sheu/rest/MemberRESTService.java
+++ src/main/java/com/sheu/rest/MemberRESTService.java
@@ -21,7 +21,6 @@ public class MemberRESTService {
     }
     @POST
     @Path("/add/payment/card")
-    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public AddPaymentCardResponse addPaymentCard(final AddPaymentCardRequest request) {
         return memberService.addPaymentCard(request);
